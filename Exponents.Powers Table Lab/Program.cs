cac61171e65a3c91b47e3b2441122e6be8807e21
carissa-c <122126939+carissa-c@users.noreply.github.com>
2023-01-11 19:38:15 -0500

    First upload

---
+++ Exponents.Powers Table Lab/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+bool runAgain = true;
+
+do
+{
+    Console.WriteLine("Please Select a number!");
+    int input = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(String.Format("{0,15} {1,15} {2,15}", "Number", "Squared", "Cubed"));
+
+    for (int num1 = 1; num1 <= input; num1++)
+    {
+        Console.WriteLine(String.Format("{0,15} {1,15} {2,15}", num1, num1 * num1, num1 * num1 * num1));
+    }
+
+    Console.WriteLine("Would you like to continue? y/n?");
+    string response = Console.ReadLine();
+
+    if (response == "y")
+    {
+        runAgain = true;
+    }
+    else
+    {
+        runAgain = false;
+    }
+    
+ }while (runAgain);
